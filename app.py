ffd486d574e258c41bfb904dbf3b742737fcab45
NmaniGiH <106422192+NmaniGiH@users.noreply.github.com>
2022-09-08 16:13:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import os
+
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    flask_app.run(debug=True, host='0.0.0.0', port=port)
+
